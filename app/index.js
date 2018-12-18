bba7168885e7437934e03292a97d9aeb2f657a7a
navira5 <navira.ali@gmail.com>
2018-12-18 10:44:48 -0600

    added svg circle

--- app/index.js
+++ app/index.js
@@ -3,11 +3,49 @@ const ReactDom = require('react-dom');
 require('./index.css');
 
 class App extends React.Component {
+constructor(props) {
+  super(props);
+  this.state = {
+    x : 500,
+    y : 500
+  };
+  this.moveIt = this.moveIt.bind(this);
+}
+
+moveIt() {
+
+    setInterval(this.setState({
+      y: this.state.y + 10
+    }), 2000)
+    
+  };
 
   render() {
     return(
       <div>
         Hello! I'm Bubbilicious. Pop me!
+        <div className="circle" >
+        < svg className = "circle"
+        height = "1000"
+        width = "1000" >
+          <circle 
+           cx = {
+             this.state.y
+           }
+           cy = {
+             this.state.y
+           }
+           r = {
+             45
+           }
+           strokeWidth = {
+             2.5
+           }
+           stroke = "#e74c3c"
+           fill = "#f1c40f"
+           />
+        </svg>
+         </div>
       </div>
     )
   }
