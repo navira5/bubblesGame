32ce234eddb84a8996fbc7cfea420b72ae908919
navira5 <navira.ali@gmail.com>
2018-12-18 16:10:07 -0600

    bubbles floating up

--- app/index.js
+++ app/index.js
@@ -8,19 +8,25 @@ class App extends React.Component {
 constructor(props) {
   super(props);
   this.state = {
-    x : 500,
-    y : 500,
-    score: 0
+    x : 200,
+    y : 200,
+    score: 0,
+    velX : 5,
+    velY : 2,
+    ballPosX : 100,
+    ballPosY : 50,
+    radius : 10,
   };
   this.moveIt = this.moveIt.bind(this);
   this.handleMouseMove = this.handleMouseMove.bind(this);
   this.handleCatchCircle = this.handleCatchCircle.bind(this);
+  this.animate = this.animate.bind(this);
 }
 
 moveIt() {
 
   setInterval(this.setState({
-      y: this.state.y + 10
+      y: this.state.y + 100
     }), 2000)
     
   };
@@ -34,10 +40,30 @@ moveIt() {
     });
   }
 
+  componentWillMount() {
+    setInterval(this.animate, 50);
+  }
+
   handleCatchCircle(e) {
     this.setState({
       score: this.state.score + 1
     });
+  }
+
+  animate() {
+      // draw ball at ballPosX, ballPosY coordinates
+      // ctx.arc(ballPosX, ballPosY, radius, 0, Math.PI * 180, false)
+      // // fill in colours etc
+
+      // // move ball
+      // ballPosX += velX;
+      // ballPosY += velY;
+
+      // // do boundary detection for bounce
+      // if (ballPosX + radius > ctx.width || ballPosX - radius < 0) {
+      //   // change velX to negative to bounce the ball the oposite X direction
+      //   velX *= -1;
+      // }
   }
 
   render() {
@@ -49,11 +75,25 @@ moveIt() {
         <div>
                 Hello! I'm Bubbilicious. Pop me!
                 Score: {this.state.score}
-                <div className="circle" >
+                <div className = "background-wrap" >
+                  <div class= "bubble x1"> </div> 
+                  <div class = "bubble x2"> </div> 
+                  <div class = "bubble x3"> </div> 
+                  <div class = "bubble x4"> </div> 
+                  <div class = "bubble x5" > </div> 
+                  <div class = "bubble x6" > </div> 
+                  <div class = "bubble x7" > </div> 
+                  <div class = "bubble x8" > </div> 
+                  <div class = "bubble x9"> </div> 
+                  <div class = "bubble x10" > </div> 
+                </div>
+                {/* <div className="circle" >
+
                 <svg className = "circle"
                 height = "1000"
                 width = "1000" >
                   <circle 
+                  className = "circle"
                    cx = {
                      this.state.y
                    }
@@ -68,10 +108,12 @@ moveIt() {
                    }
                    stroke = "#e74c3c"
                    fill = "#f1c40f"
-                   onClick = {this.handleCatchCircle}
+                   onClick = {
+                     this.moveIt
+                   }
                    />
                 </svg>
-                 </div>
+                 </div> */}
               </div>
             )
   }
