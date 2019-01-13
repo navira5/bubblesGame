f0f74eb7b801f7b509e607636808730d0ba579d5
navira5 <navira.ali@gmail.com>
2019-01-13 16:03:40 -0600

    hi

--- app/index.js
+++ app/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom';
+import Konva from 'konva';
+import data from './data'
+import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import { setInterval } from 'timers';
 require('./index.css');
 
 class App extends React.Component {
@@ -7,77 +11,160 @@ class App extends React.Component {
     super(props);
     this.state = {
       score: 0,
-      bubbles: [],
-      bubbleClass: 'bubble',
-      bubbleClasses: ["bubble x1", "bubble x2", "bubble x3", "bubble x4", "bubble x5", "bubble x6", "bubble x7", "bubble x8", "bubble x9", "bubble x10"]
-    };
-    this.addBubble = this.addBubble.bind(this);
-    this.popBubble = this.popBubble.bind(this);
+      x: 300,
+      y: 0,
+      radius: 20,
+      start: false,
+      circles: [
+        { xPos: Math.floor(Math.random() * 790) + 20, yPos: -100, radius: Math.floor(Math.random() * 20) + 5 },
+        { xPos: Math.floor(Math.random() * 790) + 20, yPos: -800, radius: Math.floor(Math.random() * 20) + 5 },
+        { xPos: Math.floor(Math.random() * 790) + 20, yPos: -40, radius: Math.floor(Math.random() * 20) + 5 },
+        { xPos: Math.floor(Math.random() * 790) + 20, yPos: -90, radius: Math.floor(Math.random() * 20) + 5 },
+        { xPos: Math.floor(Math.random() * 790) + 20, yPos: -50, radius: Math.floor(Math.random() * 20) + 5 },
+        { xPos: Math.floor(Math.random() * 790) + 20, yPos: -10, radius: Math.floor(Math.random() * 20) + 5 },
+        { xPos: Math.floor(Math.random() * 790) + 20, yPos: 0, radius: Math.floor(Math.random() * 20) + 5 },
+      ]
+    }
+
+
+    this.delete = this.delete.bind(this);
+    this.toggleStart = this.toggleStart.bind(this)
+    this.moveCircle = this.moveCircle.bind(this);
+    this.resetCircle = this.resetCircle.bind(this);
+    this.addCircle = this.addCircle.bind(this);
+    this.createCircles = this.createCircles.bind(this);
+    //this.stopPlay = this.stopPlay.bind(this);
+    this.startPlay = this.startPlay.bind(this);
   }
 
-  componentWillMount() {
-    //setInterval(this.addBubble, 2000);
+  componentDidMount() {
+    //setInterval(this.moveCircle, 20);
+    //setInterval(this.resetCircle, 10000);
+    //this.createCircles();
   }
 
-  addBubble() {
-    // this.setState({
-    //   bubbles: this.state.bubbles.concat(
-    //     <div 
-    //       onClick={() => this.popBubble()}
-    //       className={this.state.bubbleClasses[Math.floor(Math.random() * 10)]}
-    //       >
-    //       </div>)
-    // })
+  //stopPlay() {
+    //console.log('inside stop play');
+    //clearInterval(this.startGameInterval)
+  //}
+  componentDidUpdate(prevProps,  prevState) {
+    if (this.state.start !== prevState.start) {
+      if (!this.state.start) {
+        console.log('inside stop play', this);
+        clearInterval(this.startGameInterval)
+      }
+    }
+  }
+
+  startPlay() {
+    console.log('inside start play');
+    
+    this.startGameInterval = setInterval(this.moveCircle, 50);
+  }
+
+  createCircles() {
+    var interval;
+    if (this.state.circles.length < 100) {
+      setInterval(this.addCircle, 5000)
+    }
+  }
+
+  moveCircle() {
+    var copy = [...this.state.circles]
+    var newCircles = copy.map(circle => {
+      circle.yPos++;
+      return circle
+    })
+
     this.setState({
-      bubbles: this.state.bubbles.concat(this.state.bubbleClasses[Math.floor(Math.random() * 10)])
+      circles: newCircles
     })
   }
 
-  popBubble(index, bubbleClass) {
-
-    var copy = [...this.state.bubbles]
-    var filtered = copy.filter((item, i) => {
-      return i !== index;
+  resetCircle() {
+    var copy = [...this.state.circles]
+    var resetMissedBubbles = copy.map(circle => {
+      return circle.yPos < 601 ? circle : { xPos: Math.floor(Math.random() * 590) + 20, yPos: Math.floor(Math.random() * -200) + (-10), radius: Math.floor(Math.random() * 20) + 5 }
+    })
+    this.setState({
+      circles: resetMissedBubbles,
     })
 
-    var points = Number(bubbleClass[8])
+    console.log(this.state.circles)
+  }
+
+  addCircle() {
+    this.setState({
+      circles: this.state.circles.concat({ xPos: Math.floor(Math.random() * 590) + 20, yPos: Math.floor(Math.random() * -200) + (-10), radius: Math.floor(Math.random() * 20) + 5 })
+    })
+  }
+
+  toggleStart() {
+    console.log('fdfsdfds', this);
+    //clearInterval(this.startGameInterval)
+    //this.stopPlay()
+    this.setState({
+      start: !this.state.start
+    }, () => {
+        if (this.state.start) {
+          this.startPlay();
+        } else {
+          //this.stopPlay();
+        }
+    });
+  }
+
+
+  delete(e) {
+    e.target.destroy();
+
+    var newScore = this.state.score + 1
 
     this.setState({
-      bubbles: filtered,
-      score: this.state.score + points,
-      
+      score: newScore
     })
 
+    this.addCircle();
   }
-
 
   render() {
-
-    console.log('bubble arr', this.state.bubbles)
+    var startText = !this.state.start ? 'Start' : 'Pause'
     return (
       <div>
-        Hello! I'm Bubbilicious. Pop me!
-        Score: {this.state.score}
-        <div className="background-wrap" >
-          {this.state.bubbles.map((bubble, i) => {
-            return <div key={i} onClick={() => this.popBubble(i, bubble)} className={bubble}></div>;
-          })}
+        score: {this.state.score}
+        <div>
+          <button onClick={this.toggleStart}>{startText}</button>
         </div>
+
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <Text text="Pop the bubble" fontSize={15} />
+
+            {
+              this.state.circles.map((item, i) => {
+
+                var newColor = 'red'
+                if (item !== "") {
+                  return <Circle
+                    key={i}
+                    x={item.xPos}
+                    y={item.yPos}
+                    radius={item.radius}
+                    fill={newColor}
+                    onClick={this.delete}
+                    ref={node => {
+                      this.rect = node;
+                    }}
+                  />
+                }
+
+              })}
+
+          </Layer>
+        </Stage>
       </div>
     )
   }
 }
 ReactDom.render(<App />, document.getElementById('app'));
 
-/*
-<div className="bubble x1"> </div>
-<div className="bubble x2"> </div>
-<div className="bubble x3"> </div>
-<div className="bubble x4"> </div>
-<div className="bubble x5"> </div>
-<div className="bubble x6"> </div>
-<div className="bubble x7"> </div>
-<div className="bubble x8"> </div>
-<div className="bubble x9"> </div>
-<div className="bubble x10"> </div>
-          */
