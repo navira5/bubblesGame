5f17622294bb6ff01e890091d7e770dd56e7af67
navira5 <navira.ali@gmail.com>
2019-01-16 15:35:42 -0600

    add points based on size

--- app/index.js
+++ app/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Konva from 'konva';
 import { Stage, Layer, Rect, Text, Circle, Line } from 'react-konva';
+import Portal from './Portal';
 import { Slider } from 'reactrangeslider';
 import styles from './styles';
 import Play from './Play';
-import Bubble from './Bubble';
+// import Bubble from './Bubble';
 require('./index.css');
 
 class App extends React.Component {
@@ -21,55 +22,64 @@ class App extends React.Component {
         min: -20,
         max: 610
       },
-      radius: {
-        min: 5,
-        max: 50
-      },
+      radius: null,
+      xPos: Math.floor(Math.random() * (790 - 10 + 1)) + 10,
       start: false,
       speed: 50,
+      pointVal: {
+        1: 10,
+        2: 9,
+        3: 8,
+        4: 7,
+        5: 6,
+        6: 5,
+        7: 4,
+        8: 3,
+        9: 2,
+        10: 1
+      },
       circles: [
-        { xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: -800, radius: Math.floor(Math.random() * 50) + 5 },
-        { xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: -800, radius: Math.floor(Math.random() * 50) + 5 },
-        { xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: -40, radius: Math.floor(Math.random() * 50) + 5 },
-        { xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: -90, radius: Math.floor(Math.random() * 50) + 5 },
-        { xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: -100, radius: Math.floor(Math.random() * 50) + 5 },
-        { xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: -10, radius: Math.floor(Math.random() * 50) + 5 },
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, yPos: -200, radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5},
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, yPos: -50, radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5},
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, yPos: -60, radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5},
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, yPos: -90, radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5},
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, yPos: -200, radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5},
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, yPos: -150, radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5},
       ],
       divCircles: [{ yPos: 0 }, { yPos: 0 }, { yPos: 0 }, { yPos: 0 }, { yPos: 0 }]
     }
 
 
-    //this.delete = this.delete.bind(this);
+    this.delete = this.delete.bind(this);
     this.toggleStart = this.toggleStart.bind(this)
     this.moveCircle = this.moveCircle.bind(this);
-    //this.resetCircle = this.resetCircle.bind(this);
-    //this.addCircle = this.addCircle.bind(this);
+    this.resetCircle = this.resetCircle.bind(this);
+    this.addCircle = this.addCircle.bind(this);
     this.onChange = this.onChange.bind(this);
-    this.removeCircle = this.removeCircle.bind(this);
-    this.popCircle = this.popCircle.bind(this);
-    this.makeCircle = this.makeCircle.bind(this);
+    // this.removeCircle = this.removeCircle.bind(this);
+    // this.popCircle = this.popCircle.bind(this);
+    // this.makeCircle = this.makeCircle.bind(this);
   }
 
   componentDidMount() {
 
-    //setInterval(this.resetCircle, 5000);
+    setInterval(this.resetCircle, 5000);
 
-    // this.makeCircleInterval  = setInterval(this.addCircle, 2000);
-    this.makeCircleInterval = setInterval(this.makeCircle, 2000);
+    this.makeCircleInterval  = setInterval(this.addCircle, 2000);
+    //this.makeCircleInterval = setInterval(this.makeCircle, 2000);
 
   }
 
   componentDidUpdate() {
 
-    // this.state.circles.length >= 30 ? clearInterval(this.makeCircleInterval) : null;
     this.state.circles.length >= 30 ? clearInterval(this.makeCircleInterval) : null;
+    //this.state.circles.length >= 30 ? clearInterval(this.makeCircleInterval) : null;
 
   }
 
   toggleStart() {
 
     this.setState({ start: !this.state.start }, () => {
-      console.log('bbbbbbbbbb', this.state.start);
       
         this.state.start ? this.startGameInterval = setInterval(this.moveCircle, (1000 / this.state.speed)) : clearInterval(this.startGameInterval)
 
@@ -89,100 +99,83 @@ class App extends React.Component {
 
   moveCircle() {
 
-    let circlesMoved = [...this.state.divCircles].map(circle => {
-      circle.yPos += 1;
+    let circlesMoved = [...this.state.circles].map(circle => {
+      circle.yPos++;
       return circle
     })
 
-    // this.setState({ circles: newCircles })
-    this.setState({ divCircles: circlesMoved })
+    this.setState({ circles: circlesMoved })
+    //this.setState({ divCircles: circlesMoved })
   }
 
-  // resetCircle() {
-  //   let resetMissedBubbles = [...this.state.circles].map(circle => {
-  //     return circle.yPos < 601 ? circle : { xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: Math.floor(Math.random() * -200) + (-10), radius: Math.floor(Math.random() * 50) + 5 }
-  //   })
-  //   this.setState({
-  //     circles: resetMissedBubbles,
-  //   })
-  // }
-
-  // addCircle() {
-  //   this.setState({
-  //     circles: this.state.circles.concat({ xPos: Math.floor(Math.random() * window.innerWidth - 100) + 100, yPos: Math.floor(Math.random() * -200) + (-10), radius: Math.floor(Math.random() * 50) + 5 })
-  //     // circles: this.state.circles.concat({ xPos: Math.floor(Math.random() * 790) + 55, yPos: Math.floor(Math.random() * -200) + (-10), radius: Math.floor(Math.random() * 50) + 5 })
-  //   })
-  // }
-
-  // delete(e) {
-
-  //   if (this.state.start) {
-  //     e.target.destroy();
-  //   }
-  //   var newScore = this.state.score + 1
-  //   this.setState({ score: newScore })
-  //   this.addCircle();
-  // }
-
-  makeCircle() {
-
-    this.setState({ divCircles: this.state.divCircles.concat({ yPos: 0 })});
-
-  }
-
-  removeCircle(index) {
-    let afterRemove = [...this.state.divCircles].map((item, i) => {
-      return i !== index ? item : '';
+  resetCircle() {
+    let resetMissedBubbles = [...this.state.circles].map(circle => {
+      return circle.yPos < 601 ? circle : 
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, 
+          yPos: Math.floor(Math.random() * -200) + (-10), 
+          radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5 
+        }
     })
+    this.setState({ circles: resetMissedBubbles })
+  }
 
-    this.setState({ divCircles: afterRemove})
+  addCircle() {
+    this.setState({
+      circles: this.state.circles.concat(
+        { xPos: Math.floor(Math.random() * (500 - 20 + 1)) + 20, 
+          yPos: Math.floor(Math.random() * -200) + (-10), 
+          radius: Math.floor(Math.random() * (50 - 5 + 1)) + 5 
+        }
+        )
+      // circles: this.state.circles.concat({ xPos: Math.floor(Math.random() * 790) + 55, yPos: Math.floor(Math.random() * -200) + (-10), radius: Math.floor(Math.random() * 50) + 5 })
+    })
+  }
+
+  delete(e) {
+
+    const radius = e.currentTarget.attrs.radius;
+    const diameter = (radius * 2).toString()[0]
+    const scoreVal = this.state.pointVal[diameter]
+
+    this.state.start ? e.target.destroy() : null;
+  
+    var newScore = this.state.score + 1
+
+    this.setState({ score: scoreVal  })
+
+    this.addCircle();
 
   }
+
+  // makeCircle() {
+
+  //   this.setState({ divCircles: this.state.divCircles.concat({ yPos: 0 })});
+
+  // }
+
+  // removeCircle(index) {
+  //   let afterRemove = [...this.state.divCircles].map((item, i) => {
+  //     return i !== index ? item : '';
+  //   })
+
+  //   this.setState({ divCircles: afterRemove})
+
+  // }
 
  
-  popCircle(index, point) {
+  // popCircle(index, point) {
 
-    this.removeCircle(index);
+  //   this.removeCircle(index);
 
-    this.setState( { score: point})
-  }
+  //   this.setState( { score: point})
+  // }
 
   
 
 
   render() {
-    const { start, speed, score, x, y, radius, circles, divCircles} = this.state;
-    const randomSize = Math.floor(Math.random() * (100 - 10 + 1)) + 10;
-    const circleSize = randomSize.toString();
-    const point = Number(randomSize.toString()[0]);
-    const leftPos = Math.floor(Math.random() * (x.max - x.min + 1)) + x.min;
-    const pointVal = {
-      1: 10,
-      2: 9,
-      3: 8,
-      4: 7,
-      5: 6,
-      6: 5,
-      7: 4,
-      8: 3,
-      9: 2,
-      10: 1
-    };
-    //console.log('point', point, 'random size generated', randomSize, 'point value', pointVal[point])
-
-    const circleStyle = {
-      margin: '20px',
-      width: '100px',
-      height: '100px',
-      backgroundColor: 'yellow',
-      display: 'inline-block',
-      borderRadius: '50%',
-      top: '200px',
-      left: '500px',
-      position: 'absolute'
-    }
-
-    console.log('div circles', this.state.divCircles)
+    const { start, speed, score, circles } = this.state;
+     
 
     return (
       <div>
@@ -207,37 +200,16 @@ class App extends React.Component {
           </div>
         </div>
 
-
-        {/* Display moving circles */}
-
-        {divCircles.map((circle, i) => {
-          
-           <Bubble
-                    key={i}
-                    removeCircle={this.removeCircle}
-                    popCircle={this.popCircle}
-                    circleSize={circleSize}
-                    point={pointVal[point]}
-                    index={i}
-                    color={'orange'}
-                    leftPos={leftPos}
-                    topPos={circle.yPos}
-          
-          />
-        })}
-       
   
-          {/* <Stage width={window.innerWidth} height={window.innerHeight} className="stage">
+          <Stage width={window.innerWidth-164} height={window.innerHeight} className="stage">
 
             <Layer>
-              <Text fontSize={15} />
-
+  
+              
               {
-
-                this.state.circles.map((item, i) => {
-
+                circles.map((item, i) => {
                   var newColor = 'red'
-                  if (item !== "") {
+                  
                     return <Circle
                       key={i}
                       x={item.xPos}
@@ -249,15 +221,10 @@ class App extends React.Component {
                         this.rect = node;
                       }}
                     />
-                        
-             
-           
-                  }
-
                 })}
 
             </Layer>
-          </Stage> */}
+          </Stage>
         </div>
       </div>
     )
